2c273cb2c7477fb4326d93c7d416dd90b818cc06
alfonsocv12 <alfonsocvu@gmail.com>
2020-02-27 22:14:55 -0700

    Ya funcionan los usuarios

--- lib/match_menu/accounts/user.ex
+++ lib/match_menu/accounts/user.ex
@@ -1,7 +1,7 @@
 defmodule MatchMenu.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
+  import Comeonin.Bcrypt
 
   schema "users" do
     field :email, :string
@@ -29,7 +29,7 @@ defmodule MatchMenu.Accounts.User do
     case changeset do
       %Ecto.Changeset{valid?: true, changes: %{password: pass}}
         ->
-          put_change(changeset, :password_hash, hashpwsalt(pass))
+          put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(pass))
       _ ->
           changeset
     end
