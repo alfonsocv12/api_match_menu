cdf82a0f9aac8b1e00f44cbfe705ae2901ac8ed2
alfonsocv12 <alfonsocvu@gmail.com>
2020-03-03 19:00:29 -0700

    Se termino authentication de usuarios normales

--- lib/match_menu_web/router.ex
+++ lib/match_menu_web/router.ex
@@ -1,16 +1,28 @@
 defmodule MatchMenuWeb.Router do
   use MatchMenuWeb, :router
 
+  alias MatchMenu.Guardian
+  
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  pipeline :jwt_authenticated do
+    plug Guardian.AuthPipeline
+  end
+
   scope "/", MatchMenuWeb do
     pipe_through :api
-
+    IO.puts("etnro")
     get "/", BaseController, :index
 
     post "/sign_in", UserController, :sign_in
     post "/sign_up", UserController, :create
+  end
+
+  scope "/", MatchMenuWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    get "/my_user", UserController, :show
   end
 end
