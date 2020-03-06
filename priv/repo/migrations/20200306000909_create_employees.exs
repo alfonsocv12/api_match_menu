821647032b1c34a99981c18d789c1994bc8ffd40
alfonsocv12 <alfonsocvu@gmail.com>
2020-03-05 19:04:35 -0700

    Se valores a la migracion

--- priv/repo/migrations/20200306000909_create_employees.exs
+++ priv/repo/migrations/20200306000909_create_employees.exs
@@ -2,12 +2,16 @@ defmodule MatchMenu.Repo.Migrations.CreateEmployees do
   use Ecto.Migration
 
   def change do
+    create table(:employee_roll) do
+        add :roll_name, :string
+    end
+
     create table(:employees) do
       add :employee_alias, :string
       add :name, :string
       add :password_hash, :string
-      add :restaurante_id, :integer
-      add :roll_id, :integer
+      add :restaurante_id, references(:restaurants)
+      add :roll_id, references(:employee_roll)
 
       timestamps()
     end
